2b33e0d16ab769d07450875c36883034e40d68f8
harshita-079 <harshugupta0220@gmail.com>
2026-08-21 23:16:50 +0530

    last changes

--- client/lib/nutriscan-api.ts
+++ client/lib/nutriscan-api.ts
@@ -37,7 +37,7 @@
 // }
 
 const API_BASE = (
-  import.meta.env.BACKEND_URL || "http://localhost:5000"
+  import.meta.env.VITE_BACKEND_URL || "http://localhost:5000"
 ).replace(/\/$/, "");
 
 export async function analyzeProduct(file: File) {
